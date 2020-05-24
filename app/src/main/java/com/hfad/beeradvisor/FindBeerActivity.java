e38b549662c8b41bb239da672f45f477afe2564a
Tony <jahtony@hotmail.com>
2020-05-24 21:52:04 +0100

    Initial commit

---
+++ app/src/main/java/com/hfad/beeradvisor/FindBeerActivity.java
@@ -0,0 +1,35 @@
+package com.hfad.beeradvisor;
+
+// Unspecified imports were part of the project template
+
+import android.os.Bundle;
+
+// I should of added the below but leaving out for now as we have .appcompat
+// import android.app.Activity;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClickFindBeer(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+    }
